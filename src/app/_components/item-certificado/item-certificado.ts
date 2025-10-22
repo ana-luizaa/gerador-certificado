aad3bc241f9a19ca424699d5af924a44b4a59ba0
Ana <analuiza222r@gmail.com>
2025-10-22 16:01:33 -0300

    Item da lista de certificados

---
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from "../secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-item-certificado',
+  standalone: true,
+  imports: [SecondaryButtonComponent],
+  templateUrl: './item-certificado.html',
+  styleUrl: './item-certificado.css'
+})
+export class ItemCertificadoComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button';
 import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button";
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
