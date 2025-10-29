2b65afac93475b5d4e4edeeea32e25d244061b60
Ana <analuiza222r@gmail.com>
2025-10-29 13:11:50 -0300

    Usando RouterLink para navegação entre as páginas

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../secondary-button/secondary-button";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-item-certificado',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css'
 })
 export class ItemCertificadoComponent {
-
+  id: string = '6'
 }
--- src/app/pages/certificado/certificado.ts
+++ src/app/pages/certificado/certificado.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificado',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificado.html',
   styleUrl: './certificado.css'
 })
