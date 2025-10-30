faf88d01d909ebb0aaf987f07ed8e3f51bc521e6
Ana <analuiza222r@gmail.com>
2025-10-30 11:34:32 -0300

    Usando o NgModel no formulário

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button";
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoFormComponent {
-
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Cobol'];
 }
